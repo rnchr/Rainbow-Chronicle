e9871dcdced0934c58ca11e273e92fcc2bc67c3a
Andrew Purcell <andrew.purcell@tufts.edu>
2012-03-15 00:10:51 -0400

    small change to rating

--- app/models/place_rating.rb
+++ app/models/place_rating.rb
@@ -1,6 +1,5 @@
 class PlaceRating < ActiveRecord::Base
   belongs_to :user
   belongs_to :place
-  belongs_to :rating
   attr_accessible :ip_address, :created_at, :user_id, :overall, :review, :comment
 end
